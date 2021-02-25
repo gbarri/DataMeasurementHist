385eb17ae4184eccad6734e29549da55c9ff23db
Barri Giacomo <g.barri@reply.it>
2021-02-25 01:53:57 +0100

    aggiornamento endpoint

--- src/main/java/com/reply/labcamp/H2OSpoon/Controller.java
+++ src/main/java/com/reply/labcamp/H2OSpoon/Controller.java
@@ -6,10 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
+import javax.xml.ws.Response;
 import java.util.Map;
 
 @RestController
@@ -26,13 +30,37 @@ public class Controller {
     PTO8 titania;
 
     @GetMapping("benzene")
-    public Map<String, Double> getBenzene48hours(){
-        return benzene.listBenzeneValues();
+    public ResponseEntity<Map<String, Double>> getBenzene48hours(){
+        return ResponseEntity.ok(benzene.listBenzeneValues());
     }
 
     @GetMapping("titania")
-    public Map<String, Double> getTitania48Hours(){
-        return titania.litsOtherValues();
+    public ResponseEntity<Map<String, Double>> getTitania48Hours(){
+        return ResponseEntity.ok(titania.litsOtherValues());
     }
+
+    @GetMapping("benzene/{lagi}")
+    public ResponseEntity<Double> getBenzeneLag(@PathVariable("lagi") String lagi){
+        Map<String, Double> map = benzene.listBenzeneValues();
+        logger.debug(map.toString());
+        String key = "lag"+lagi;
+        if(map.containsKey(key)){
+            return ResponseEntity.ok(map.get(key));
+        }else {
+            throw new IllegalArgumentException("lag not found, please select from lag1 to lag48");
+        }
+    }
+
+    @GetMapping("titania/{lagi}")
+    public ResponseEntity<Double> getTitaniaLag(@PathParam("lagi") String lagNumber){
+        Map<String, Double> map = titania.litsOtherValues();
+        String key = "lag"+lagNumber;
+        if(map.containsKey(key)){
+            return ResponseEntity.ok(map.get(key));
+        }else {
+            throw new IllegalArgumentException("lag not found, please select from lag1 to lag48");
+        }
+    }
+
 
 }
